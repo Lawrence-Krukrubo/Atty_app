723c54aea089f66660ef83c9fb202ed78db2fa6b
Lawrence-Krukrubo <sisokels@gmail.com>
2022-06-05 17:06:46 +0100

    docs: updating main.py

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
-from connect import connection, cursor, query_to_df
 import streamlit as st
 from datetime import date, datetime
+import mysql
+from mysql.connector import Error
+import pandas as pd
+import time
+
+db_name = 'dand'
+connection = mysql.connector.connect(host='localhost',
+                                         database=db_name,
+                                         user= 'danam',
+                                         password= 'roots')
+
+cursor = connection.cursor()
+cursor.execute("select database();")
+record = cursor.fetchone()
+print("You're connected to database: ", record)
+
+
+def query_to_df(query):
+    """This method converts the result
+        of a SQL query to a DataFrame.
+    @param query: Is a string of valid SQL syntax
+    """
+
+    st = time.time()
+    # Assert Every Query ends with a semi-colon
+    try:
+        assert query.endswith(';')
+    except AssertionError:
+        return 'ERROR: Query Must End with ;'
+
+    # so we never have more than 50 rows displayed
+    pd.set_option('display.max_rows', 30)
+    df = None
+
+    # Process the query
+    cursor.execute(query)
+    columns = cursor.description
+    result = []
+    for value in cursor.fetchall():
+        tmp = {}
+        for (index, column) in enumerate(value):
+            tmp[columns[index][0]] = [column]
+        result.append(tmp)
+
+    # Create a DataFrame from all results
+    for ind, data in enumerate(result):
+        if ind >= 1:
+            x = pd.DataFrame(data)
+            df = pd.concat([df, x], ignore_index=True)
+        else:
+            df = pd.DataFrame(data)
+    print(f'Query ran for {time.time() - st} secs!')
+    return df
+
 
 st.title('DAND Connect Session Attendance')
 st.subheader('Session Lead: Lawrence Krukrubo')
